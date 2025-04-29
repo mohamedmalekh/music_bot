d46a30ecbfdf36f722c8437c757fd6888b44dbf4
m.m.h <124166343+mohamedmalekh@users.noreply.github.com>
2025-04-29 18:01:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,6 +111,7 @@ YOUTUBE_CHANNELS = [
     "https://www.youtube.com/channel/UC7JpmOJscJcm-VsF6XnvYWg",
     "https://www.youtube.com/channel/UC8bEqtGJEUoYdWnti8k3R2Q",
     "https://www.youtube.com/channel/UC-bIUmH8gFA_lFkV2rJ6GMQ",
+"https://www.youtube.com/channel/UC5BYACI8gIm3wDlVnhKFG5A",
     
 ]
 
