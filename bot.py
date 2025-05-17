340569a018d04695c7845f0891731ff29023d78d
m.m.h <124166343+mohamedmalekh@users.noreply.github.com>
2025-05-17 20:46:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,6 +135,7 @@ YOUTUBE_CHANNELS = [
 "https://www.youtube.com/channel/UC4m5L8brApVSVe_AoD_Lw4w",
 "https://www.youtube.com/channel/UCwKKwo3yWoVfPjlKN62KqQA",
 "https://www.youtube.com/channel/UCU6cE7pdJPc6DU2jSrKEsdQ",
+"https://www.youtube.com/channel/UCByOQJjav0CUDwxCk-jVNRQ",
 ]
 
 # ==== Historique ====
